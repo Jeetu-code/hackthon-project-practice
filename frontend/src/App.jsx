58e3694e1d2849e86fad4741250723f4158e9d72
jeetu-code <jk1871881@gmail.com>
2026-02-18 21:37:46 +0530

    barcode check in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import {useRef,useEffect,useState} from "react";
-
-
-
 function App() {
-const [devices,setDevices] = useState([]);
 const videoRef = useRef(null);
 const [front,setFront] = useState(false);
+const [barcode,setBarcode]=useState("barcode is supported");
+const [barcodeDector,setBarcodeDector]=useState(false);
 useEffect(()=>{
 let stream;
 async function startcamera(){
@@ -14,10 +12,33 @@ try{
 stream=await navigator.mediaDevices.getUserMedia({video:{facingMode:front?"user":"environment"}});
 videoRef.current.srcObject =stream;
 }catch(err){
-cosnole.log("camera error :",err);
+console.log("camera error :",err);
 }
 }
 startcamera();
+
+// check compatibility
+if (!("BarcodeDetector" in globalThis)) {
+	setBarcode("Barcode Detector is not supported by this browser.");
+  console.log("Barcode Detector is not supported by this browser.");
+} else {
+  console.log("Barcode Detector supported!");
+
+  // create new detector
+  const barcodeDetector = new BarcodeDetector({
+    formats: ["code_39", "codabar", "ean_13"],
+  });
+barcodeDetector
+  .detect(imageEl)
+  .then((barcodes) => {
+    barcodes.forEach((barcode) => console.log(barcode.rawValue));
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+}
+
+
 return ()=>{
 if(stream){stream.getTracks().forEach((track)=>(track.stop()));}
 };
@@ -30,6 +51,8 @@ return (
 <br/>
 <br/>
 <video ref={videoRef} autoPlay playsInline/>
+
+<div>{barcode}</div>
 </>
 );
 }
