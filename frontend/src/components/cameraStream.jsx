db7e5005392013eee42a161bde5b302855ee4f66
jeetu-code <jk1871881@gmail.com>
2026-02-18 07:46:18 +0530

    updated useEffect of cameraStream.jsx

--- frontend/src/components/cameraStream.jsx
+++ frontend/src/components/cameraStream.jsx
@@ -4,29 +4,32 @@ import {useEffect,useRef} from "react";
 export default function CameraStream({deviceId}){
 const videoRef = useRef(null);
 console.log(deviceId);
-useEffect(()=>{
-async function startCamera(){
-if(!deviceId) return;
-try{
-await navigator.mediaDevices.getUserMedia({video:true});
-const stream = await navigator.mediaDevices.getUserMedia({video:{deviceId:{exact:deviceId}}});
-videoRef.current.srcObject=stream;
-}
-catch(err){
-console.log("Camera access error :",err);
-}
-}
-startCamera();
 
-return ()=>{
-if(videoRef.current?.srcObject){
-videoRef.current.srcObject
-	.getTracks()
-	.forEach((track)=>track.stop());
-}
-};
-},[deviceId]);
+useEffect(() => {
+  if (!deviceId) return;
 
+  let stream;
+
+  async function startCamera() {
+    try {
+      stream = await navigator.mediaDevices.getUserMedia({
+        video: { facingMode: deviceId }
+      });
+
+      videoRef.current.srcObject = stream;
+    } catch (err) {
+      console.log("Camera error:", err);
+    }
+  }
+
+  startCamera();
+
+  return () => {
+    if (stream) {
+      stream.getTracks().forEach(track => track.stop());
+    }
+  };
+}, [deviceId]);
 return (
 <video ref={videoRef} autoPlay playsInline/>
 );
